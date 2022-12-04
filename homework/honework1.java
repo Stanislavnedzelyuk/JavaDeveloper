80320441fefb3163fb11e7bcf9b9f92f32cb59b9
Stanislav <stanislav.nedzelyuk@icloud.com>
2022-12-04 21:48:47 +0700

    0.3

--- homework/honework1.java
+++ homework/honework1.java
@@ -2,7 +2,41 @@ package homework;
 
 public class honework1 {
     public static void main(String[] args) {
-        int x =5;
-        System.out.println(x);
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1 () {
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+    }
+
+    public static void task2 () {
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+        dog = dog*4;
+        cat = cat*4;
+        paper = paper*4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+    }
+    public static void task3 () {
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+        dog = (int) (dog-3.5);
+        cat = cat-1.6;
+        paper = paper-7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
     }
 }

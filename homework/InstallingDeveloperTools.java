e787ed18912f7725b957447877f890f7177b58b0
Stanislav <stanislav.nedzelyuk@icloud.com>
2022-12-05 12:18:04 +0700

    Вводный урок

--- homework/InstallingDeveloperTools.java
+++ homework/InstallingDeveloperTools.java
@@ -1,6 +1,6 @@
 package homework;
 
-public class homework0 {
+public class InstallingDeveloperTools { // Установка инструментов разработчика
     public static void main(String[] args) {
         var x =5;
         System.out.println(x);
